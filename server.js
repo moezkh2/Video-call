a3a699acaca5818bc84ad127439767faeeec30da
moezkh2 <khorchani_moez_eddine@outlook.com>
2021-12-14 21:04:06 +0100

    heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
-
+require('dotenv').config()
 const io = require("socket.io")(server, {
     cors: {
         origin: "*",
@@ -13,9 +13,9 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
     res.send('Running');
-});
+}); */
 
 io.on("connection", (socket) => {
     socket.emit("me", socket.id);
@@ -32,5 +32,13 @@ io.on("connection", (socket) => {
         io.to(data.to).emit("callAccepted", data.signal)
     });
 });
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.json())
+    app.use(express.static('./client/build'))
+    app.get('/*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+    })
+}
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
