91861ccc20c0ef3dd1b7fc8abd2b0a2348a20fe6
moezkh2 <khorchani_moez_eddine@outlook.com>
2021-12-16 19:12:16 +0100

    fix server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const express = require('express')
 const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
@@ -34,7 +35,6 @@ io.on("connection", (socket) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.json())
     app.use(express.static('./client/build'))
     app.get('/*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
